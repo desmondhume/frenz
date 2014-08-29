ffb0c40f3ee614ac66b3603a17190547004034f9
Gregorio Setti <gregorio.setti@gmail.com>
2014-08-29 23:06:21 +0200

    Setup specs config

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
+  describe "with fb_uid and token" do
 
+    describe "post #create" do
+      post :create, fb_uid: '123123', token: 'asdoasdo'
+
+      it "returns welcome message" do
+        expect(json).to have_key('message')
+      end
+
+      it { should respond_with 200 }
+    end
+  end
 end
