2f90bd7ee22180223e30cd2f5374ea8daab1a64f
Gregorio Setti <gregorio.setti@gmail.com>
2014-08-30 01:32:47 +0200

    First tests for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  
+  post 'api/v1/users', to: 'users#create'
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,13 +4,17 @@ RSpec.describe UsersController, :type => :controller do
   describe "with fb_uid and token" do
 
     describe "post #create" do
-      post :create, fb_uid: '123123', token: 'asdoasdo'
 
       it "returns welcome message" do
+        post :create, format: :json, fb_uid: '123123', token: 'asdoasdo'
         expect(json).to have_key('message')
+        expect(json['message']).to eq('Welcome!')
       end
 
-      it { should respond_with 200 }
+      it "should respond with success" do
+        post :create, format: :json, fb_uid: '123123', token: 'asdoasdo'
+        expect(response).to have_http_status 201
+      end
     end
   end
 end
