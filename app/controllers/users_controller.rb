bb72a5073cd6b75259eac91e7cb9afee79b4a621
Gregorio Setti <gregorio.setti@gmail.com>
2014-08-30 19:08:16 +0200

    Refactor json output formatting in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,14 @@
 class UsersController < ApplicationController
-
+  respond_to :json
   before_filter :require_login, only: [:me]
 
   def me
-    respond_to do |format|
-      format.json { render json: {user: @user}.to_json, status: 200 }
-    end
+    respond_with @user, status: 200
   end
 
   def index
     @users = User.all
-    respond_to do |format|
-      format.json { render json: {users: @users}.to_json, status: 200 }
-    end
+    respond_with users: @users, status: 200
   end
 
   private
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe UsersController, :type => :controller do
     it "should return a list of users" do
       FactoryGirl.create(:user)
       FactoryGirl.create(:john_travolta)
+
       get :index, format: :json
+      
       expect(json).to have_key('users')
       expect(json['users'].count).to eq(2)
     end
