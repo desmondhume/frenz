0272b008f56e20015963746e26a0f231e9f0e632
Gregorio Setti <gregorio.setti@gmail.com>
2014-08-30 02:07:18 +0200

    Add koala as facebook sdk

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
   def create
     respond_to do |format|
-      @user = {
-            first_name: 'Gregorio',
-            last_name: 'Setti',
-            town: 'Milano'
-          }
-      format.json { render json: {message: 'Welcome!', user: @user}.to_json, status: 201 }
+      parse_facebook_cookies
+      @access_token = @facebook_cookies["access_token"]
+      format.json { render json: {message: 'Welcome!', user: @access_token}.to_json, status: 201 }
     end
   end
+
+  private
+
+    def parse_facebook_cookies
+      @facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
+    end
+
 end
