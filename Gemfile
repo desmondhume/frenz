f11429d4e83f3e662808314ffa7dd17819acf03a
Gregorio Setti <gregorio.setti@gmail.com>
2014-08-30 02:07:18 +0200

    Add koala as facebook sdk

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
+gem 'koala'
+
 group :test do
   gem 'cucumber-rails', :require => false
   gem 'database_cleaner'
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe UsersController, :type => :controller do
         post :create, format: :json, 
           fb_uid: '123123', 
           token: 'asdoasdo'
-
+          puts json
         expect(json).to have_key('message')
         expect(json).to have_key('user')
       end
