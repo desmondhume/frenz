41a24f1d0ac034c6c8a4561cc67fe75dcb743d02
Gregorio Setti <gregorio.setti@gmail.com>
2014-08-30 12:42:08 +0200

    User tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates_uniqueness_of :fb_uid
+  validates_uniqueness_of :access_token
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+
+  it { should validate_uniqueness_of(:fb_uid) }
+  it { should validate_uniqueness_of(:access_token) }
   
 end
