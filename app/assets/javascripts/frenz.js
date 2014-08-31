f4033a2a3c7e6de05a6279c71443a40bf954431f
Gregorio Setti <gregorio.setti@gmail.com>
2014-08-31 14:48:13 +0200

    User serializer

--- app/assets/javascripts/frenz.js
+++ app/assets/javascripts/frenz.js
@@ -10,16 +10,9 @@ window.Frenz = {
     var _this = this;
     this.session = new this.Models.Session();
     this.current_user = new _this.Models.User();
+    
     if(this.session.authenticated()) {
       _this.current_user.me();
-    } else {
-      $('#login_with_facebook').on('click', function() {
-        _this.session.login(function(response) {
-          localStorage.setItem("frenz_access_token", response.get('user')['access_token']);
-          _this.session.set('accessToken', localStorage.getItem("frenz_access_token"));
-          _this.current_user.me();
-        });
-      })
     }
 
     this.mainView = new Frenz.Views.Main();
